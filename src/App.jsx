2a738fd8691ac8629c048a71989f7a1cec9ec3bc
KevinSebaLee <48044115@est.ort.edu.ar>
2025-04-23 22:40:33 -0300

    Finalizar TP

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,42 @@
-import './App.css'
-import TarjetaCita from './components/tarjetaCita/tarjetaCita'
+import './App.css';
+import { useState } from 'react';
+import TarjetaCita from './components/tarjetaCita/tarjetaCita';
+import CrearCita from './components/crearCita/crearCita';
 
 function App() {
+  const [citas, setCitas] = useState([]);
+
+  const agregarCita = (nuevaCita) => {
+    setCitas([...citas, nuevaCita]);
+  };
+
   return (
     <>
       <h1>Administrador de Pacientes</h1>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <h1>Crear mi cita</h1>
+            <CrearCita crearCita={agregarCita} />
+          </div>
 
-      
-      <div>
-        <h1>Administra tus citas</h1>
-        <TarjetaCita nombre="Toro" dueño="Juan" fecha="20/12/2025" hora="12:00" sintomas="Dolor de panza"/>
+          <div className="one-half column">
+            <h1>Administra tus citas</h1>
+            {citas.map((cita, numeroCitas) => (
+              <TarjetaCita 
+                key={numeroCitas}
+                nombre={cita.nombre}
+                dueño={cita.dueño}
+                fecha={cita.fecha}
+                hora={cita.hora}
+                sintomas={cita.sintomas}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     </>
   );
 }
 
-export default App
+export default App;
