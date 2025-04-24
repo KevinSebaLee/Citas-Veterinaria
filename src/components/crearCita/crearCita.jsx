3e70543979ffa44c285c45d3b8307753882b2475
KevinSebaLee <48044115@est.ort.edu.ar>
2025-04-23 22:41:49 -0300

    Agregar required a los input

--- src/components/crearCita/crearCita.jsx
+++ src/components/crearCita/crearCita.jsx
@@ -32,19 +32,19 @@ const CrearCita = ({ crearCita }) => {
     return (
         <form onSubmit={handleSubmit}>
             <label>Nombre Mascota</label>
-            <input type="text" name="nombre" className="u-full-width" placeholder="Nombre Mascota" value={form.nombre} onChange={handleChange} />
+            <input type="text" name="nombre" className="u-full-width" placeholder="Nombre Mascota" value={form.nombre} onChange={handleChange} required/>
 
             <label>Nombre Dueño</label>
-            <input type="text" name="dueño" className="u-full-width" placeholder="Nombre dueño de la mascota" value={form.dueño} onChange={handleChange} />
+            <input type="text" name="dueño" className="u-full-width" placeholder="Nombre dueño de la mascota" value={form.dueño} onChange={handleChange} required/>
 
             <label>Fecha</label>
-            <input type="date" name="fecha" className="u-full-width" value={form.fecha} onChange={handleChange} />
+            <input type="date" name="fecha" className="u-full-width" value={form.fecha} onChange={handleChange} required/>
 
             <label>Hora</label>
-            <input type="time" name="hora" className="u-full-width" value={form.hora} onChange={handleChange} />
+            <input type="time" name="hora" className="u-full-width" value={form.hora} onChange={handleChange} required/>
 
             <label>Síntomas</label>
-            <textarea name="sintomas" className="u-full-width" value={form.sintomas} onChange={handleChange}></textarea>
+            <textarea name="sintomas" className="u-full-width" value={form.sintomas} onChange={handleChange} required></textarea>
 
             <button type="submit" className="u-full-width button-primary">Agregar Cita</button>
         </form>
